1890cf58612b2904c1d18c062c55c566ad1b0e5f
doeg <hi@doeg.gy>
2018-03-30 12:12:41 -0400

    Stub template/ (#1)
    * Add directory & Makefile
    
    * Add includes for address, joypad, and timer constants
    
    * Add header & main.asm loop
    
    * rm unused constants

---
+++ template/src/main.asm
@@ -0,0 +1,24 @@
+include "addrs.inc"
+
+SECTION  "Vblank", ROM0[$0040]
+  reti
+SECTION  "LCDC", ROM0[$0048]
+  reti
+SECTION  "Timer_Overflow", ROM0[$0050]
+  reti
+SECTION  "Serial", ROM0[$0058]
+  reti
+SECTION  "p1thru4", ROM0[$0060]
+  reti
+
+; Point-of-entry
+SECTION  "start", ROM0[$0100]
+  nop
+  jp main
+
+SECTION "main", ROMX
+
+main::
+  nop
+.loop
+  jp .loop
