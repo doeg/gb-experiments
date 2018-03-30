696d42304096f66ea4c01d07ff5e847b9295326c
Sara Bee <hi@doeg.gy>
2018-03-30 12:52:45 -0400

    Non-BCD addition loop

--- bcd/src/main.asm
+++ bcd/src/main.asm
@@ -13,12 +13,26 @@ SECTION  "p1thru4", ROM0[$0060]
 
 ; Point-of-entry
 SECTION  "start", ROM0[$0100]
+
   nop
   jp main
+
+SECTION "variables", WRAM0
+COUNTER:: ds 1
 
 SECTION "main", ROMX
 
 main::
   nop
+.reset
+  xor a
+  ld [COUNTER], a
+
+; Counts up from 0, in units of 19
 .loop
+  ld a, [COUNTER]
+  ld b, a
+  ld a, $13 ; 19
+  add a, b
+  ld [COUNTER], a
   jp .loop
