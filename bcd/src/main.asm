518559a82aae2523e457b3abfa600c62eccc21de
Sara Bee <hi@doeg.gy>
2018-03-31 10:38:15 -0400

    Generalize counter loop for 3 digits

--- bcd/src/main.asm
+++ bcd/src/main.asm
@@ -18,9 +18,9 @@ SECTION  "start", ROM0[$0100]
   jp main
 
 SECTION "variables", WRAM0
-COUNTER:: ds 2
-COUNTER_BYTES EQU 2
-COUNTER_INCR EQU 1 ; 5
+COUNTER:: ds 3
+COUNTER_BYTES EQU 3
+COUNTER_INCR EQU $10 ; 5
 
 SECTION "main", ROMX
 
@@ -37,23 +37,24 @@ main::
   dec b
   jr nz, .reset_counter_loop
 
-; Count up from 0
-.counter_loop
+.counter_start
+  ld c, COUNTER_INCR
   ld hl, COUNTER
+
+; c - increment for 1s digit
+; hl - address of 1byte BCD digit pair to increment
+.counter_loop
   ld a, [hl]
   ld b, a
-  ld a, COUNTER_INCR
+  ld a, c
   add a, b
   daa
   ld [hl], a
   jr nc, .counter_loop_done
-; Increment the 10s place digit by 1
-.counter_loop_next_digit
   inc l
-  ld a, [hl]
-  add a, 1
-  daa
-  ld [hl], a
+  ld c, 1
+  jr .counter_loop
 .counter_loop_done::
   nop
-  jr .counter_loop
+  ; TODO update screen here
+  jr .counter_start
