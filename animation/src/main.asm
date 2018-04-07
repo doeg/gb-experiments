6a99bcc41426ff1571ca2629424e9c088666c91d
Sara Bee <hi@doeg.gy>
2018-04-07 12:56:48 -0400

    Load gengar tiles to VRAM

--- animation/src/main.asm
+++ animation/src/main.asm
@@ -32,6 +32,8 @@ TILE_SIZE_BYTES EQU 16
 ; Each sprite is composed of 4 tiles
 SPRITE_SIZE_BYTES EQU TILE_SIZE_BYTES * 4
 
+pGENGAR_TILES EQU $81a0
+
 SECTION "main", ROMX
 init::
   nop
@@ -57,6 +59,12 @@ init::
 .set_interrupts_enabled
   ld a, %00000001
   ld [pINTERRUPT_ENABLE], a
+
+.load_tiles
+  ld bc, gengar ; source
+  ld hl, pGENGAR_TILES  ; dest
+  ld de, SPRITE_SIZE_BYTES * 2   ; size
+  call memcpy
 
 .screen_on
   ei
@@ -102,13 +110,33 @@ on_vblank::
   pop af
   reti
 
-gengar_0::
+; de - block size
+; bc - source address
+; hl - destination address
+memcpy::
+  dec de
+.memcpy_loop:
+  ld a, [bc]
+  ld [hl], a
+  inc bc
+  inc hl
+  dec de
+.memcpy_check_limit:
+  ld a, e
+  cp $00
+  jr nz, .memcpy_loop
+  ld a, d
+  cp $00
+  jr nz, .memcpy_loop
+  ret
+
+gengar::
+  ; Frame 0
   db $00, $00, $00, $00, $00, $00, $00, $00, $32, $32, $3f, $3f, $3f, $3f, $1f, $1b
   db $00, $00, $00, $00, $00, $00, $00, $00, $4c, $4c, $fc, $fc, $fc, $fc, $f8, $d8
   db $3f, $39, $3f, $3f, $7d, $72, $7d, $7a, $3d, $3e, $1f, $1f, $1f, $1f, $0c, $0c
   db $fc, $9c, $fc, $fc, $be, $4e, $be, $5e, $bc, $7c, $f8, $f8, $f8, $f8, $30, $30
-
-gengar_1::
+  ; Frame 1
   db $00, $00, $00, $00, $00, $00, $00, $00, $04, $04, $3f, $3f, $3f, $3f, $1f, $1f
   db $00, $00, $00, $00, $00, $00, $00, $00, $20, $20, $fc, $fc, $fc, $fc, $f8, $f8
   db $3f, $3b, $3f, $39, $3f, $3f, $7d, $7a, $7d, $7e, $3f, $3f, $1f, $1f, $0f, $0f
